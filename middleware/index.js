f6e42a9c16688b65b32eaa80dff57ed7eb923459
frinzekt <frinzekt@gmail.com>
2020-08-14 16:13:19 +0800

    middleware configure function comment

--- middleware/index.js
+++ middleware/index.js
@@ -1,3 +1,4 @@
+//THIS EXPORTS A CONFIGURE FUNCTION JUST LIKE MOST OF THE EXPORT HERE
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   // Add your custom middleware here. Remember that
