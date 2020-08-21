62a4d76a900df4f5085b1c1cd97c8beb102ef22d
frinzekt <frinzekt@gmail.com>
2020-08-21 16:04:00 +0800

    github oauth strategy

--- authentication.js
+++ authentication.js
@@ -1,13 +1,30 @@
-const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
-const { LocalStrategy } = require('@feathersjs/authentication-local');
-const { expressOauth } = require('@feathersjs/authentication-oauth');
+const {
+  AuthenticationService,
+  JWTStrategy,
+} = require("@feathersjs/authentication");
+const { LocalStrategy } = require("@feathersjs/authentication-local");
+const {
+  expressOauth,
+  OAuthStrategy,
+} = require("@feathersjs/authentication-oauth");
 
-module.exports = app => {
+class GitHubStrategy extends OAuthStrategy {
+  // EXTENDS FUNCTIONALITY OF THE GET ENTITY
+  async getEntityData(profile) {
+    const baseData = await super.getEntityData(profile);
+
+    // INCULDE BOTH THE EMAIL, AND THE BASEDATA
+    return { ...baseData, email: profile.email };
+  }
+}
+
+module.exports = (app) => {
   const authentication = new AuthenticationService(app);
 
-  authentication.register('jwt', new JWTStrategy());
-  authentication.register('local', new LocalStrategy());
+  authentication.register("jwt", new JWTStrategy());
+  authentication.register("local", new LocalStrategy());
+  authentication.register("github", new GitHubStrategy());
 
-  app.use('/authentication', authentication);
+  app.use("/authentication", authentication);
   app.configure(expressOauth());
 };
