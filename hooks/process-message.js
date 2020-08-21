cdfb839e7f75f0c3c6343e82a1cbb5d6e1de1a21
frinzekt <frinzekt@gmail.com>
2020-08-21 15:04:33 +0800

    process message before

--- hooks/process-message.js
+++ hooks/process-message.js
@@ -3,7 +3,27 @@
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
-  return async context => {
+  return async (context) => {
+    const { data } = context;
+
+    if (!data.text) {
+      throw new Error("A message must have text");
+      // IF ERROR IS THROWN, IT WILL STOP ALL THE INCOMING HOOKS AND WILL JUMP STRAIGHT TO THE CLIENT
+      // IN SIMPLER TERMS, IT WON'T RUN THE NEXT HOOKS
+    }
+
+    // AUTHENTICATED WILL ALWAYS HAVE USER DATA ON THE PARAMS OBJECT
+    const { user } = context.params;
+
+    const text = data.text.substring(0, 400);
+
+    //  OVERWRITING WHAT wILL BE SAVED IN THE DB
+    context.data = {
+      text,
+      userId: user._id,
+      createdAt: new Date().getTime(),
+    };
+
     return context;
   };
 };
