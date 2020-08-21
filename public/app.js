4a62cd2f14dbf409803e918ed83d2e1c3634afeb
frinzekt <frinzekt@gmail.com>
2020-08-21 16:21:54 +0800

    basic login html interface

--- public/app.js
+++ public/app.js
@@ -35,3 +35,43 @@ const main = async () => {
 };
 
 main();
+
+// Login screen
+const loginHTML = `<main class="login container">
+  <div class="row">
+    <div class="col-12 col-6-tablet push-3-tablet text-center heading">
+      <h1 class="font-100">Log in or signup</h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-12 col-6-tablet push-3-tablet col-4-desktop push-4-desktop">
+      <form class="form">
+        <fieldset>
+          <input class="block" type="email" name="email" placeholder="email">
+        </fieldset>
+
+        <fieldset>
+          <input class="block" type="password" name="password" placeholder="password">
+        </fieldset>
+
+        <button type="button" id="login" class="button button-primary block signup">
+          Log in
+        </button>
+
+        <button type="button" id="signup" class="button button-primary block signup">
+          Sign up and log in
+        </button>
+
+        <a class="button button-primary block" href="/oauth/github">
+          Login with GitHub
+        </a>
+      </form>
+    </div>
+  </div>
+</main>`;
+
+const showLogin = () => {
+  document.getElementById("app").innerHTML = loginHTML;
+};
+
+showLogin();
