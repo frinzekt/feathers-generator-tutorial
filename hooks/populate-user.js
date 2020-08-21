a550f9a61129b8f716d52d52ca0f70e2691e91a3
frinzekt <frinzekt@gmail.com>
2020-08-21 15:11:12 +0800

    populate user after hook for message service

--- hooks/populate-user.js
+++ hooks/populate-user.js
@@ -3,7 +3,26 @@
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
-  return async context => {
+  return async (context) => {
+    const { app, method, result, param } = context;
+
+    // ATTACHING USER IN MESSAGE
+    const addUser = async (message) => {
+      const user = await app.service("users").get(messsage.userId, params);
+      return {
+        ...message,
+        user,
+      };
+    };
+
+    // WILL NEED TO CHANGE ALL IF THERE ARE MULTIPLE MESSAGES
+    if (method === "find") {
+      result.data = await Promise.all(result.data.map(addUser));
+    } else {
+      //  one message: get, create, update
+      context.result = await addUser(result);
+    }
+
     return context;
   };
 };
